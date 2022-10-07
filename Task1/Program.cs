02b6b53b9f3b4185686f4880032720480683745e
 mac <kksanaa@mail.ru>
2022-10-07 16:16:36 +0300

    task1

---
+++ Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+
+void AnswerToTask(int number)
+{
+    if (number >= 10000 && number < 100000)
+    {
+        string numericAsString = number.ToString();
+
+
+        if (numericAsString[0] == numericAsString[4] && numericAsString[1] == numericAsString[3])
+        {
+            Console.WriteLine($"{number} - Палиндром");
+        }
+        else
+        {
+            Console.WriteLine($"{number} - НЕ палиндром");
+        }
+    }
+    else
+        Console.WriteLine($"{number} - не является пятизначным");
+
+}
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+AnswerToTask(number);
+
+
+
+
